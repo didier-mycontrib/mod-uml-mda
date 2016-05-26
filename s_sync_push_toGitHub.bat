d6e64a5e50b7d2030b565d3a321ec8e48f2cd536
formation <formation@ici_ou_la.fr>
2016-05-26 16:41:22 +0200

    with good github url

--- s_sync_push_toGitHub.bat
+++ s_sync_push_toGitHub.bat
@@ -1,4 +1,4 @@
-git remote add GitHubMyContribOrigin https://github.com/didier-mycontrib/mod_uml_mda.git
+git remote add GitHubMyContribOrigin https://github.com/didier-mycontrib/mod-uml-mda.git
 REM didier-mycontrib / gh14.....sm..x / didier@d-defrance.fr
 git push -u GitHubMyContribOrigin master
 pause
